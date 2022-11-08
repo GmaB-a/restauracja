b05b71eff4889660effd203d690dbe5193d897f0
GmaB-a <vladik11012006@gmail.com>
2022-11-08 19:37:13 +0100

    burger sprites uploaded + burger scriptable objects made

---
+++ Assets/Scripts/Burgers/BurgerScriptableObjects.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BurgerData", menuName = "ScriptableObjects/BurgerData", order = 1)]
+public class BurgerScriptableObjects : ScriptableObject
+{
+    public Sprite BurgerSprite;
+    public bool HasOnion;
+    public bool HasTomato;
+    public bool HasCheese;
+    public bool HasSalad;
+    public bool HasPatty;
+}
